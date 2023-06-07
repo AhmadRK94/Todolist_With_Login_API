5d85712dd998273b888469ea6f5bde76a512ba77
AhmadR <arkarami94@gmail.com>
2023-06-07 23:19:17 +0330

    handling cors

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
-from fastapi import FastAPI, HTTPException, Depends, status
-from . import models
+from fastapi import FastAPI
 from .db import engine
 from .routers import todo, user, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user.router)
 app.include_router(todo.router)
 app.include_router(auth.router)
-
-
-@app.get("/")
-def root():
-    return {"Hello": "Welcome to my api"}
